258b23f602173fa34de5406dbe70b9558e05f97d
Crowdbotics <team@crowdbotics.com>
2020-10-30 16:02:44 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -4,7 +4,6 @@ export const installed_blueprints = [
 { name: 'BlankScreen313592', human_name: 'Blank Screen 3', access_route: 'BlankScreen313592'},
 { name: 'BlankScreen813540', human_name: 'Blank Screen 8', access_route: 'BlankScreen813540'},
 { name: 'BlankScreen713539', human_name: 'Blank Screen 7', access_route: 'BlankScreen713539'},
-{ name: 'BlankScreen513538', human_name: 'Blank Screen 5', access_route: 'BlankScreen513538'},
 { name: 'BlankScreen613537', human_name: 'Blank Screen 6', access_route: 'BlankScreen613537'},
 { name: 'BlankScreen413534', human_name: 'Blank Screen 4', access_route: 'BlankScreen413534'},
 { name: 'BlankScreen213532', human_name: 'Blank Screen 2', access_route: 'BlankScreen213532'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -9,7 +9,6 @@ import BlankScreen413593Navigator from '../features/BlankScreen413593/navigator'
 import BlankScreen313592Navigator from '../features/BlankScreen313592/navigator';
 import BlankScreen813540Navigator from '../features/BlankScreen813540/navigator';
 import BlankScreen713539Navigator from '../features/BlankScreen713539/navigator';
-import BlankScreen513538Navigator from '../features/BlankScreen513538/navigator';
 import BlankScreen613537Navigator from '../features/BlankScreen613537/navigator';
 import BlankScreen413534Navigator from '../features/BlankScreen413534/navigator';
 import BlankScreen213532Navigator from '../features/BlankScreen213532/navigator';
@@ -26,7 +25,6 @@ BlankScreen413593: { screen: BlankScreen413593Navigator },
 BlankScreen313592: { screen: BlankScreen313592Navigator },
 BlankScreen813540: { screen: BlankScreen813540Navigator },
 BlankScreen713539: { screen: BlankScreen713539Navigator },
-BlankScreen513538: { screen: BlankScreen513538Navigator },
 BlankScreen613537: { screen: BlankScreen613537Navigator },
 BlankScreen413534: { screen: BlankScreen413534Navigator },
 BlankScreen213532: { screen: BlankScreen213532Navigator },
